58553814a9340a8360cf9c1d6a186449ab14371a
NagasawaYusei <curga3013@gmail.com>
2023-01-18 20:35:09 +0900

    ジャンプするようにした

--- Assets/Scripts/Player/PlayerMove.cs
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -10,6 +10,9 @@ public class PlayerMove : MonoBehaviour
     [Tooltip("プレイヤースピードの乗数")]
     float _playerMultipleSpeed = 10f;
 
+    [SerializeField]
+    float _playerJumpPower = 10f;
+
     [Tooltip("インプットシステムのコンポーネント"), SerializeField]
     PlayerInput _playerInput;
 
@@ -19,21 +22,45 @@ public class PlayerMove : MonoBehaviour
 
     Vector2 _moveDir;
 
+    bool _isJump;
+
+    [SerializeField]
+    LayerMask _groundLayer;
+
+    [SerializeField]
+    Vector3 _groundDetectionSize;
+
+    [SerializeField]
+    Vector3 _groundDetectionCentor;
+
+    Vector3 _playerCentor;
+
+    [SerializeField]
+    bool _isGizmo;
+
     #region イベント登録
     private void OnEnable()
     {
         _playerInput.onActionTriggered += OnMove;
+        _playerInput.onActionTriggered += OnJump;
     }
 
     private void OnDisable()
     {
         _playerInput.onActionTriggered -= OnMove;
+        _playerInput.onActionTriggered -= OnJump;
     }
     #endregion
 
     void Start()
     {
         SetUp();
+    }
+
+    void Update()
+    {
+        State();
+        Jump();
     }
 
     void FixedUpdate()
@@ -44,12 +71,12 @@ public class PlayerMove : MonoBehaviour
     void SetUp()
     {
         //RequireComponent知ってるけどデバッグを出したいからこうする
-        if(!TryGetComponent(out _rb))
+        if (!TryGetComponent(out _rb))
         {
             Debug.LogWarning("Rbないよ～");
             _rb = gameObject.AddComponent<Rigidbody>();
         }
-        
+
         _transform = GetComponent<Transform>();
     }
 
@@ -60,10 +87,65 @@ public class PlayerMove : MonoBehaviour
         _rb.AddForce((dir.normalized * _playerMoveSpeed * _playerMultipleSpeed) + _rb.velocity.y * Vector3.up, ForceMode.Acceleration);
     }
 
-    private void OnMove(InputAction.CallbackContext context)
+    //今度プレイヤーステート的なクラスを作るから引っ越します
+    void State()
+    {
+        _playerCentor = _transform.position + _groundDetectionCentor;
+    }
+
+    void Jump()
+    {
+        if(_isJump && IsGround())
+        {
+            _rb.AddForce(Vector3.up * _playerJumpPower);
+            _isJump = false;
+        }
+    }
+
+    //今度プレイヤーステート的なクラスを作るから引っ越します
+    public bool IsGround()
+    {
+        Collider[] collision = Physics.OverlapBox(_playerCentor, _groundDetectionSize, Quaternion.identity, _groundLayer);
+        if (collision.Length != 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        if (_isGizmo)
+        {
+            Gizmos.DrawCube(_playerCentor, _groundDetectionSize);
+        }
+    }
+
+    void OnMove(InputAction.CallbackContext context)
     {
         if (context.action.name != "Move")
             return;
+
         _moveDir = context.ReadValue<Vector2>();
+    }
+
+    void OnJump(InputAction.CallbackContext context)
+    {
+        if (context.action.name != "Jump")
+            return;
+
+        if (context.started)
+        { 
+            _isJump = true;
+        }
+
+        if (context.canceled)
+        {
+            _isJump = false;
+        }
     }
 }
